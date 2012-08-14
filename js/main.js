4da60c7af0428b421de7abe61c9667da762baa30
mbikt <kyaw@blueprint.org>
2012-08-14 14:21:42 +0800

    clean up

--- js/main.js
+++ js/main.js
@@ -1,8 +1,4 @@
 
 goog.require('ydn.json');
 goog.require('ydn.object');
-goog.require('ydn.http');
-goog.require('ydn.http.AccessTokenTransport');
-goog.require('ydn.http.ProxyTransport');
-goog.require('ydn.http.GseTransport');
 
