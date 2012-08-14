2e39b5aac43b5e8e2b443730d4c974989cc688eb
mbikt <kyaw@blueprint.org>
2012-08-14 14:12:19 +0800

    reorgnizing http

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 
 goog.require('ydn.json');
 goog.require('ydn.object');
+goog.require('ydn.http');
+goog.require('ydn.http.AccessTokenTransport');
+goog.require('ydn.http.ProxyTransport');
+goog.require('ydn.http.GseTransport');
+
