cfd9ebd155753b3224835116720ac9fdb625f044
mbikt <kyaw@blueprint.org>
2012-09-11 08:49:55 +0800

    build system update

---
+++ externs/browser.js
@@ -0,0 +1,83 @@
+/**
+ * @fileoverview About this file.
+ * User: mbikt
+ * Date: 4/24/12
+ * @externs
+ */
+
+/**
+ *
+ * @type {Object}
+ */
+var JSON = {};
+
+
+/**
+ *
+ * @param {string} str
+ * @return {Object}
+ */
+JSON.parse = function (str) {};
+
+/**
+ *
+ * @param {Object} obj
+ * @return {string}
+ */
+JSON.stringify = function (obj) {};
+
+
+/**
+* Socket IO
+* @constructor
+*/
+function Socket() {}
+
+/**
+* @param {string} channel
+* @param {Function} listener
+*/
+Socket.prototype.on = function(channel, listener) {};
+
+/**
+* @param {string} channel
+* @param {Object} obj
+*/
+Socket.prototype.emit = function(channel, obj) {};
+
+/**
+* Socket IO service
+* @constructor
+*/
+function io() {}
+
+/**
+* @param {string} server
+* @return {Socket}
+*/
+io.prototype.connect = function(server) {};
+
+
+/**
+ * New IndexedDB interface not in closure externs.
+ * This must be remove when they updated.
+ * @constructor
+ */
+
+/**
+ * @extends {IDBRequest}
+ * @constructor
+ */
+var IDBOpenDBRequest = function() {};
+
+/**
+ *
+ * @param ev
+ */
+IDBOpenDBRequest.prototype.onblocked = function(ev) {};
+
+/**
+ *
+ * @param ev
+ */
+IDBOpenDBRequest.prototype.onupgradeneeded = function(ev) {};
---
+++ externs/misc.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Extern declaration for JsTestDriver that is not in closure testing suite.
+ * User: mbikt
+ * Date: 4/18/12
+ * @see js_test_driver
+ * @externs
+ */
+
+/**
+ *
+ * @param {string} name
+ */
+var TestCase = function(name) {};
+
+/**
+ *
+ * @param {string} name
+ */
+var AsyncTestCase = function(name) {};
+
+
+var assertException = function (msg, callback, error) { // in Closure test, it is assertThrows
+};
+
+var jstestdriver = {};
+
+
+
+
+
+
+
+
+
