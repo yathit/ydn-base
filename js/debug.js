069e01a6b40b2ba67fcddbeb9e46f5015e32b6e7
kyaw <kyaw@blueprint.org>
2012-12-21 09:53:01 +0800

    seperate ydn-base exports

---
+++ js/debug.js
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: kyawtun
+ * Date: 12/21/12
+ * Time: 9:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+goog.require('ydn.debug');
+
+
+goog.exportSymbol('ydn.debug.log', ydn.debug.log);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 
-goog.require('ydn.json');
-goog.require('ydn.object');
 
+goog.require('goog.async.Deferred');
+
+goog.exportProperty(goog.async.Deferred.prototype, 'done',
+  goog.async.Deferred.prototype.addCallback);
+goog.exportProperty(goog.async.Deferred.prototype, 'fail',
+  goog.async.Deferred.prototype.addErrback);
+goog.exportProperty(goog.async.Deferred.prototype, 'then',
+  goog.async.Deferred.prototype.addCallbacks);
