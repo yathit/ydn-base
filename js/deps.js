1bcff1e7429a1755ca49d425d3c95a19e3e9f227
kyawtun <okisan.2008@gmail.com>
2013-02-25 22:32:49 +0800

    fixing composite query in websql.

--- js/deps.js
+++ js/deps.js
@@ -17,7 +17,7 @@ goog.addDependency("../../../ydn-base/js/http/static_mock_server.js", ['ydn.http
 goog.addDependency("../../../ydn-base/js/http/transport.js", ['ydn.http.ITransport', 'ydn.http.Transport', 'ydn.http.CallbackResult'], []);
 goog.addDependency("../../../ydn-base/js/http/xmlhttpreq_transport.js", ['ydn.http.XMLHttpRequest'], ['ydn.http.ITransport', 'ydn.http.Transport', 'ydn.http', 'goog.object', 'goog.Uri', 'ydn.debug.error.ArgumentException']);
 goog.addDependency("../../../ydn-base/js/ydn/async.js", ['ydn.async'], ['goog.async.Deferred', 'goog.async.DeferredList']);
-goog.addDependency("../../../ydn-base/js/ydn/debug.js", ['ydn.debug', 'ydn.debug.error.ArgumentException', 'ydn.debug.error.NotSupportedException'], ['goog.debug.Console', 'goog.debug.Logger', 'goog.debug.LogManager', 'goog.debug.DivConsole']);
+goog.addDependency("../../../ydn-base/js/ydn/debug.js", ['ydn.debug', 'ydn.debug.error.ArgumentException', 'ydn.debug.error.NotSupportedException', 'ydn.debug.error.NotImplementedException'], ['goog.debug.Console', 'goog.debug.Logger', 'goog.debug.LogManager', 'goog.debug.DivConsole']);
 goog.addDependency("../../../ydn-base/js/ydn/error.js", ['ydn.error', 'ydn.error.ArgumentException', 'ydn.error.NotImplementedException', 'ydn.error.ConstrainError', 'ydn.error.NotSupportedException', 'ydn.error.InternalError', 'ydn.error.InvalidOperationException', 'ydn.error.InvalidOperationError'], []);
 goog.addDependency("../../../ydn-base/js/ydn/expression.js", ['ydn.math.Expression'], ['ydn.string', 'goog.string', 'ydn.object', 'ydn.error']);
 goog.addDependency("../../../ydn-base/js/ydn/json.js", ['ydn.json'], ['goog.debug.Logger']);
