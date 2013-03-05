f6b6959ed7d2f6287663540ae08756453fea465f
kyaw <kyaw@blueprint.org>
2013-03-05 16:28:40 +0800

    config folder.

---
+++ js/config/main.js
@@ -0,0 +1,16 @@
+
+goog.provide('ydn.base');
+goog.require('goog.async.Deferred');
+goog.require('goog.events.EventTarget');
+
+goog.exportProperty(goog.async.Deferred.prototype, 'done',
+  goog.async.Deferred.prototype.addCallback);
+goog.exportProperty(goog.async.Deferred.prototype, 'fail',
+  goog.async.Deferred.prototype.addErrback);
+goog.exportProperty(goog.async.Deferred.prototype, 'then',
+  goog.async.Deferred.prototype.addCallbacks);
+goog.exportProperty(goog.async.Deferred.prototype, 'always',
+  goog.async.Deferred.prototype.addBoth);
+
+goog.exportProperty(goog.events.EventTarget.prototype, 'addEventListener',
+  goog.events.EventTarget.prototype.addEventListener);
