2cce974f29ff9e2efd18cf9f0f8a681aaaf16e91
kyaw <kyaw@blueprint.org>
2013-03-05 09:44:26 +0800

    all test update.

--- tools/alltests.py
+++ tools/alltests.py
@@ -8,9 +8,11 @@ import os.path
 import sys
 
 
-def add_test_files(test_files, dirname, names):
+def add_test_files(arg, dirname, names):
 	"""File names that end in "_test.html" are added to test_files."""
 
+	test_files = arg['files']
+	ext = arg['ext']
 	root_dir = sys.argv[2]
 
 	#print 'WALK: ' + wpath
@@ -19,8 +21,8 @@ def add_test_files(test_files, dirname, names):
 		path = os.path.join(dirname, name)
 		if os.path.isdir(path):
 			#print "extending path: %s" % wpath
-			test_files.extend(find_test_files(path))
-		elif os.path.isfile(path) and name.endswith('_test.html'):
+			test_files.extend(find_test_files(path, ext))
+		elif os.path.isfile(path) and name.endswith(ext):
 
 			d = os.path.join(dirname.replace('\\', '/'), str(name))
 			wpath = d[len(root_dir)+1:len(d)]
@@ -29,15 +31,23 @@ def add_test_files(test_files, dirname, names):
 			#print pathJsArg + ' n ' + str(name)
 			test_files.append(pathJsArg)
 
-def find_test_files(directory):
+def find_test_files(directory, ext = '_test.html'):
 	"""Returns the list of files in directory that end in "_test.html"."""
 	if not os.path.exists(directory) or not os.path.isdir(directory):
 		raise Exception('Not a directory: ' + directory)
 
 	test_files = []
-	os.path.walk(directory, add_test_files, test_files)
+	os.path.walk(directory, add_test_files, {'files': test_files, 'ext': ext})
 
 	return test_files
+
+def write_test_files(f, base_dir, ext = '_test.html', var = '_allTests'):
+	"""Find test file and write to the file."""
+
+	test_files = find_test_files(base_dir, ext)
+	f.write('var %s = [' % var)
+	test_files = set(test_files)
+	f.write(', '.join(test_files) + '];\n')
 
 def usage():
 	"""Displays a message to the user when invalid input is supplied.
@@ -53,11 +63,12 @@ def main():
 	else:
 		base_dir = sys.argv[2]
 
-		test_files = find_test_files(base_dir)
 		f = open(sys.argv[1], 'w')
-		f.write('var _allTests = [')
-		test_files = set(test_files)
-		f.write(', '.join(test_files) + '];')
+		if (len(sys.argv) > 3 and sys.argv[3] == 'db'):
+			write_test_files(f, base_dir, '_idb_test.html', '_idb_allTests')
+			write_test_files(f, base_dir, '_websql_test.html', '_websql_allTests')
+			write_test_files(f, base_dir, '_ls_test.html', '_ls_allTests')
+		write_test_files(f, base_dir, '_test.html', '_allTests')
 		f.close()
 		print "All test file saved to %s" % sys.argv[1]
 		sys.exit(0)
