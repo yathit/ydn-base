5c94357ec57a1e4149f19b55a151036970f6c488
kyaw <kyaw@blueprint.org>
2013-04-03 11:40:39 +0800

    simple storage primary key range query implemented.

--- js/ydn/error.js
+++ js/ydn/error.js
@@ -8,7 +8,7 @@
 goog.provide('ydn.error');
 goog.provide('ydn.error.ArgumentException');
 goog.provide('ydn.error.NotImplementedException');
-goog.provide('ydn.error.ConstrainError');
+goog.provide('ydn.error.ConstraintError');
 goog.provide('ydn.error.NotSupportedException');
 goog.provide('ydn.error.InternalError');
 goog.provide('ydn.error.InvalidOperationException');
@@ -131,11 +131,11 @@ ydn.error.InternalError.prototype.name = 'ydn.InternalError';
  * @constructor
  * @extends {Error}
  */
-ydn.error.ConstrainError = function(opt_msg) {
+ydn.error.ConstraintError = function(opt_msg) {
 
   // Ensure there is a stack trace.
   if (Error.captureStackTrace) {
-    Error.captureStackTrace(this, ydn.error.ConstrainError);
+    Error.captureStackTrace(this, ydn.error.ConstraintError);
   } else {
     this.stack = new Error().stack || '';
   }
@@ -143,11 +143,11 @@ ydn.error.ConstrainError = function(opt_msg) {
   if (opt_msg) {
     this.message = String(opt_msg);
   }
-  this.name = 'ydn.error.ConstrainError';
+  this.name = 'ydn.error.ConstraintError';
 };
-goog.inherits(ydn.error.ConstrainError, Error);
+goog.inherits(ydn.error.ConstraintError, Error);
 
-//ydn.error.ConstrainError.prototype.name = 'ydn.ConstrainError';
+ydn.error.ConstraintError.prototype.name = 'ydn.error.ConstraintError';
 
 
 
