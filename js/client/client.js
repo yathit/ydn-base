c680368f878caf1e1e6742b6b463d43c0bab7da9
kyaw <kyaw@blueprint.org>
2013-07-05 17:25:16 +0800

    responddata protected.

--- js/client/client.js
+++ js/client/client.js
@@ -36,52 +36,125 @@ ydn.client.getXhrManager = function() {
  * Create HTTP Raw Response data.
  * This class exists so that it is easier to interpolate with GAPI library and
  * provide strong type, when use internally.
- * @param {number} status
- * @param {*} body
+ * @param {number|string} status status or raw.
+ * @param {*=} opt_body
  * @param {Object.<string>=} opt_headers
  * @param {string=} opt_status_text
  * @constructor
  * @struct
  */
-ydn.client.HttpRespondData = function(status, body, opt_headers,
+ydn.client.HttpRespondData = function(status, opt_body, opt_headers,
                                       opt_status_text) {
-  this.status = status;
-  this.body = body;
-  this.headers = opt_headers || {};
-  this.statusText = opt_status_text;
+  if (goog.isString(status)) {
+    this.raw = status;
+  } else {
+    this.raw = null;
+    this.status = status;
+    this.body = opt_body;
+    this.headers = opt_headers || {};
+    this.statusText = opt_status_text;
+  }
 };
 
 
 /**
+ * Unparsed raw result.
+ * @type {string?}
+ * @protected
+ */
+ydn.client.HttpRespondData.prototype.raw;
+
+
+/**
  * @type {*}
+ * @protected
  */
 ydn.client.HttpRespondData.prototype.body;
 
 
 /**
  * @type {!Object.<string>}
+ * @protected
  */
 ydn.client.HttpRespondData.prototype.headers;
 
 
 /**
  * @type {number}
+ * @protected
  */
 ydn.client.HttpRespondData.prototype.status;
 
 
 /**
  * @type {string|undefined}
+ * @protected
  */
 ydn.client.HttpRespondData.prototype.statusText;
 
 
 /**
- * @param {gapi.client.RawResp|ydn.client.HttpRespondData} args
+ * @protected
+ */
+ydn.client.HttpRespondData.prototype.ensureParse = function() {
+  if (this.raw) {
+    var json = ydn.json.parse(this.raw);
+    if (json['gapiRequest']) {
+      json = json['gapiRequest']['data'];
+    }
+    this.status = json['status'];
+    this.statusText = json['statusText'];
+    this.headers = json['headers'];
+    this.body = json['body'];
+    this.raw = null;
+  }
+};
+
+
+/**
+ * @return {number}
+ */
+ydn.client.HttpRespondData.prototype.getStatus = function() {
+  this.ensureParse();
+  return this.status;
+};
+
+
+/**
+ * @return {string}
+ */
+ydn.client.HttpRespondData.prototype.getStatusText = function() {
+  this.ensureParse();
+  return this.statusText || '';
+};
+
+
+/**
+ * @return {*}
+ */
+ydn.client.HttpRespondData.prototype.getBody = function() {
+  this.ensureParse();
+  return this.body;
+};
+
+
+/**
+ * @param {string} header Header name.
+ * @return {string|undefined}
+ */
+ydn.client.HttpRespondData.prototype.getHeader = function(header) {
+  this.ensureParse();
+  return this.headers[header];
+};
+
+
+/**
+ * @param {gapi.client.RawResp|ydn.client.HttpRespondData|string} args
  * @return {!ydn.client.HttpRespondData}
  */
 ydn.client.HttpRespondData.wrap = function(args) {
   return args instanceof ydn.client.HttpRespondData ? args :
+      goog.isString(args) ? new ydn.client.HttpRespondData(args) :
       new ydn.client.HttpRespondData(args['status'], args['body'],
           args['headers'], args['statusText']);
 };
