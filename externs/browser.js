616333ab86bb05bf923c473d189f5eb4a91edcda
kyaw <kyaw@blueprint.org>
2013-08-14 13:48:08 +0800

    doc update

--- externs/browser.js
+++ externs/browser.js
@@ -151,3 +151,53 @@ IDBFactory.prototype.cmp = function(first, second) {};
  * @const
  */
 IDBRequest.prototype.error;
+
+
+
+/**
+ * @constructor
+ */
+function DataTransferItem() {}
+
+
+/**
+ * @type {string}
+ */
+DataTransferItem.prototype.kind;
+
+
+/**
+ * @type {string}
+ */
+DataTransferItem.prototype.type;
+
+
+/**
+ * @return {File?}
+ */
+DataTransferItem.prototype.getAsFile = function() {};
+
+
+/**
+ * @type {Array.<DataTransferItem>}
+ */
+DataTransfer.prototype.items;
+
+
+
+/**
+ * The drag-and-drop processing model involves several events.
+ * http://www.whatwg.org/specs/web-apps/current-work/multipage/dnd.html#the-dragevent-interface
+ * @constructor
+ * @extends {MouseEvent}
+ */
+function DragEvent() {}
+
+
+/**
+ * @type {DataTransfer}
+ */
+DragEvent.prototype.dataTransfer;
+
+
+
