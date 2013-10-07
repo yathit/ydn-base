59c0272d702e5bd9fd1e04ae6a8655a01fbf14dc
kyaw <kyaw@blueprint.org>
2013-10-07 13:02:26 +0800

    reporter

--- src/ydn/xml/utils.js
+++ src/ydn/xml/utils.js
@@ -8,20 +8,23 @@ goog.provide('ydn.xml.utils');
 goog.require('goog.dom.xml');
 
 
+
 /**
  * Convert from XML to JSON format
  *
+ *
  * @param {Element|Document|Node|string} xml XML data.
- * @param {string=} opt_root_xmlns root xmlns name space.
+ * @param {(string|boolean)=} format This conversion assume xml is GData Atom
+ * format.
  * @return {Object|*} JSON object.
  */
-ydn.xml.utils.xml2json = function(xml, opt_root_xmlns) {
+ydn.xml.utils.xml2json = function(xml, format) {
   // this code is based on http://davidwalsh.name/convert-xml-json
 
   // see http://code.google.com/apis/gdata/docs/json.html for Google
   // specification about conversion
-  var is_atom_format = opt_root_xmlns == 'atom';
-  var is_plain = !opt_root_xmlns;
+  var is_atom_format = format == 'atom' || format === true;
+  var is_plain = format == 'plain';
 
   /**
    * If an element has a namespace alias, the alias and element are concatenated
@@ -65,19 +68,18 @@ ydn.xml.utils.xml2json = function(xml, opt_root_xmlns) {
       if (is_plain && nodeName == '$t') {
         obj = item.nodeValue;
       } else if (!goog.isDef(obj[nodeName])) {
-        obj[nodeName] = ydn.xml.utils.xml2json(item, opt_root_xmlns);
+        obj[nodeName] = ydn.xml.utils.xml2json(item, format);
       } else {
         if (!goog.isArray(obj[nodeName])) {
           var old = obj[nodeName];
           obj[nodeName] = [];
           obj[nodeName].push(old);
         }
-        obj[nodeName].push(ydn.xml.utils.xml2json(item, opt_root_xmlns));  // ?
+        obj[nodeName].push(ydn.xml.utils.xml2json(item, format));  // ?
       }
     }
   }
   return obj;
 };
-
 
 
