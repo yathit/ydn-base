feb16b58f8a07391d57c9e30f07d21c0ae385e91
kyaw <kyaw@blueprint.org>
2013-10-29 13:18:12 +0800

    use custom deferred without try/catch block

--- src/config/main.js
+++ src/config/main.js
@@ -1,20 +1,13 @@
 
 goog.provide('ydn.base.exports');
 goog.require('ydn.async.Deferred');
-goog.require('ydn.base');
 
 goog.exportProperty(goog.async.Deferred.prototype, 'done',
     goog.async.Deferred.prototype.addCallback);
 goog.exportProperty(goog.async.Deferred.prototype, 'fail',
     goog.async.Deferred.prototype.addErrback);
-if (ydn.base.JQUERY) {
-  goog.exportProperty(ydn.async.Deferred.prototype, 'then',
-      ydn.async.Deferred.prototype.then);
-} else {
-  goog.exportProperty(goog.async.Deferred.prototype, 'then',
-      goog.async.Deferred.prototype.addCallbacks);
-}
-
 goog.exportProperty(goog.async.Deferred.prototype, 'always',
     goog.async.Deferred.prototype.addBoth);
+goog.exportProperty(ydn.async.Deferred.prototype, 'then',
+    ydn.async.Deferred.prototype.then);
 
