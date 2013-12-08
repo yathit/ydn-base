fbc4ae0ece93a84b60d3b51e26e77fef8314f451
kyawtun <okisan.2008@gmail.com>
2013-12-08 15:00:55 +0800

    sugar fetch module working.

--- src/client/simple-client.js
+++ src/client/simple-client.js
@@ -12,12 +12,18 @@ goog.require('ydn.client.SimpleHttpRequest');
 /**
  * Singleton simple client.
  * @param {goog.net.XhrManager=} opt_xm xhr manager.
+ * @param {Object=} opt_headers optional headers to send with each request.
  * @constructor
  * @implements {ydn.client.Client}
  */
-ydn.client.SimpleClient = function(opt_xm) {
+ydn.client.SimpleClient = function(opt_xm, opt_headers) {
   this.xm_ = goog.isDef(opt_xm) ? // use default only if not null.
       opt_xm : ydn.client.getXhrManager();
+  /**
+   * @type {Object|undefined}
+   * @private
+   */
+  this.header_ = opt_headers;
 };
 
 
@@ -32,6 +38,14 @@ ydn.client.SimpleClient.prototype.xm_;
  * @inheritDoc
  */
 ydn.client.SimpleClient.prototype.request = function(args) {
+  if (this.header_) {
+    if (!args.headers) {
+      args.headers = {};
+    }
+    for (var key in this.header_) {
+      args.headers[key] = this.header_[key];
+    }
+  }
   return new ydn.client.SimpleHttpRequest(args, this.xm_);
 };
 
