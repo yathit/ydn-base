1655e1cf3ac50b590920e7ce2f870eb44cf5c821
kyawtun <okisan.2008@gmail.com>
2013-12-25 15:56:44 +0800

    canvas icon

---
+++ externs/kineticjs.js
@@ -0,0 +1,34 @@
+/**
+ * @fileoverview Extens for KineticJS
+ */
+
+
+/**
+ * @type {Object}
+ * @const
+ */
+var Kinetic = {};
+
+
+
+/**
+ * @param {{container: string, width: number, height: number}} params
+ * @constructor
+ */
+Kinetic.Layer = function(params) {};
+
+
+
+/**
+ * @param {{data: string, fill: string, scale: number, x: number, y: number}} params
+ * @constructor
+ */
+Kinetic.Path = function(params) {};
+
+
+
+/**
+ * @param {{scale: number}} params
+ * @constructor
+ */
+Kinetic.Group = function(params) {};
