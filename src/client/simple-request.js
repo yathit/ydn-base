f0c5a2a1626930560f45d4893ed796261699acd5
Kyaw <kyaw@blueprint.org>
2013-12-26 16:20:08 +0800

    contact panel working

--- src/client/simple-request.js
+++ src/client/simple-request.js
@@ -69,10 +69,10 @@ ydn.client.SimpleHttpRequest.prototype.xm_;
 /**
  * Execute the request.
  * @param {function(this: T, Object, ydn.client.HttpRespondData)?} cb
- * @param {T=} opt_obj scope.
+ * @param {T=} opt_scope scope.
  * @template T
  */
-ydn.client.SimpleHttpRequest.prototype.execute = function(cb, opt_obj) {
+ydn.client.SimpleHttpRequest.prototype.execute = function(cb, opt_scope) {
   goog.asserts.assert(this.xm_, this + ' already executed.');
   var data = this.req_data;
   var url = new goog.Uri(data.path);
@@ -81,20 +81,6 @@ ydn.client.SimpleHttpRequest.prototype.execute = function(cb, opt_obj) {
   }
   var callback = function(e) {
     var xhr = /** @type {goog.net.XhrIo} */ (e.target);
-    var body = xhr.getResponse();
-    var json = null;
-    var is_json = true; // todo
-    if (is_json) {
-      if (goog.isObject(body)) {
-        json = body;
-      } else if (goog.isString(body)) {
-        try {
-          json = ydn.json.parse(body);
-        } catch (je) {
-          json = body;
-        }
-      }
-    }
     var header_lines = xhr.getAllResponseHeaders().split('\n');
     var headers = {};
     for (var i = 0; i < header_lines.length; i++) {
@@ -104,10 +90,27 @@ ydn.client.SimpleHttpRequest.prototype.execute = function(cb, opt_obj) {
         headers[name] = header_lines[i].substr(idx + 1).trim();
       }
     }
+    var body = xhr.getResponse();
+    var json = body;
+    var is_json = headers['content-type'] && /json/.test(headers['content-type']);
+    if (is_json) {
+      if (goog.isObject(body)) {
+        json = body;
+      } else if (goog.isString(body)) {
+        /**
+         * @preserveTry
+         */
+        try {
+          json = ydn.json.parse(body);
+        } catch (je) {
+          json = body;
+        }
+      }
+    }
     var resp = new ydn.client.HttpRespondData(xhr.getStatus(), body, headers,
         xhr.getStatusText());
     if (cb) {
-      cb.call(opt_obj, /** @type {Object} */ (json), resp);
+      cb.call(opt_scope, /** @type {Object} */ (json), resp);
       cb = null;
     }
   };
