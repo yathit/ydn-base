03172cb2cbe75537c13f110b3427165d1d72faa5
kyawtun <okisan.2008@gmail.com>
2013-12-29 20:28:28 +0800

    metadata base ui

--- externs/sugarcrm.js
+++ externs/sugarcrm.js
@@ -68,3 +68,97 @@ SugarCrm.Entry = function() {};
  * @type {string}
  */
 SugarCrm.NameValue.prototype.id;
+
+
+
+/**
+ * Represents SugarCRM ModuleField.
+ * @constructor
+ */
+SugarCrm.ModuleField = function() {};
+
+
+/**
+ * @type {boolean}
+ */
+SugarCrm.ModuleField.prototype.calculated;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleField.prototype.group;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleField.prototype.id_name;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleField.prototype.label;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleField.prototype.len;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleField.prototype.name;
+
+
+/**
+ * @type {Object.<SugarCrm.NameValue>}
+ */
+SugarCrm.ModuleField.prototype.options;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleField.prototype.related_module;
+
+
+/**
+ * @type {number}
+ */
+SugarCrm.ModuleField.prototype.required;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleField.prototype.type;
+
+
+
+/**
+ * Represents SugarCRM ModuleInfo.
+ * @constructor
+ */
+SugarCrm.ModuleInfo = function() {};
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleInfo.prototype.module_name;
+
+
+/**
+ * @type {string}
+ */
+SugarCrm.ModuleInfo.prototype.table_name;
+
+
+/**
+ * @type {Object.<SugarCrm.ModuleField>}
+ */
+SugarCrm.ModuleInfo.prototype.module_fields;
