506f3574f699ac806dddbe0542bace9b9611b715
Kyaw Tun <kyawtun@yathit.com>
2014-08-17 12:19:33 +0800

    add record ui unit test, reveal header.

--- src/ydn/mock-chrome-extension.js
+++ src/ydn/mock-chrome-extension.js
@@ -3,14 +3,17 @@
  */
 
 
+goog.provide('ydn.testing.mockExtension');
+
+
 /**
  * @define {string} extension origin
  */
-_CHROME_EXTENSION_BASE_URL = '';
+ydn.testing.mockExtension.BASE_URL = '';
 
 
-if (!chrome) {
-  chrome = {};
+if (!window.chrome) {
+  window.chrome = {};
 }
 
 
@@ -18,15 +21,15 @@ if (!chrome.extension) {
   chrome.extension = {
     'getURL': function(s) {
       if (!s) {
-        return _CHROME_EXTENSION_BASE_URL;
-      } else if (!s.charAt(0) != '/' &&
-          _CHROME_EXTENSION_BASE_URL.charAt(0) != '/') {
+        return ydn.testing.mockExtension.BASE_URL;
+      } else if (s.charAt(0) != '/' &&
+          ydn.testing.mockExtension.BASE_URL.charAt(0) != '/') {
         s = '/' + s;
       } else if (s.charAt(0) == '/' &&
-          _CHROME_EXTENSION_BASE_URL.charAt(0) == '/') {
+          ydn.testing.mockExtension.BASE_URL.charAt(0) == '/') {
         s = s.substring(1);
       }
-      return _CHROME_EXTENSION_BASE_URL + s;
+      return ydn.testing.mockExtension.BASE_URL + s;
     }
   };
 }
