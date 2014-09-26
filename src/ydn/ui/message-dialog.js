58d9794c9eb2ec0bf7631e628e6caf1cbbd2c084
Kyaw Tun <kyawtun@yathit.com>
2014-09-26 17:50:46 +0800

    dialog box

--- src/ydn/ui/message-dialog.js
+++ src/ydn/ui/message-dialog.js
@@ -21,17 +21,17 @@ ydn.ui.MessageDialog = function(title, message, btn) {
    * @type {HTMLDialogElement}
    * @private
    */
-  this.dialog_ = document.createElement('dialog');
+  this.dialog_ = /** @type {HTMLDialogElement} */ (document.createElement('dialog'));
   var header = document.createElement('div');
   header.className = 'header';
   var content = document.createElement('div');
-  header.className = 'content';
+  content.className = 'content';
   var button_bar = document.createElement('div');
-  header.className = 'button-bar';
+  button_bar.className = 'button-bar';
 
   var title_el = document.createElement('h3');
   title_el.textContent = title;
-  header.appendChild(header);
+  header.appendChild(title_el);
 
   if (goog.isString(message)) {
     var msg_el = document.createElement('div');
@@ -42,11 +42,15 @@ ydn.ui.MessageDialog = function(title, message, btn) {
   }
 
   var default_btn = document.createElement('button');
-  default_btn.className = 'ok';
-  default_btn.setAttribute('value', 'apply');
+  default_btn.className = 'ok default';
+  default_btn.value = 'ok';
   default_btn.textContent = 'OK';
   button_bar.appendChild(default_btn);
 
+  this.dialog_.appendChild(header);
+  this.dialog_.appendChild(content);
+  this.dialog_.appendChild(button_bar);
+  this.dialog_.className = ydn.ui.MessageDialog.CSS_CLASS_NAME;
   document.body.appendChild(this.dialog_);
 
 };
@@ -62,6 +66,12 @@ ydn.ui.MessageDialog.Button = {
 
 
 /**
+ * @define {string} default class name
+ */
+ydn.ui.MessageDialog.CSS_CLASS_NAME = 'ydn-crm';
+
+
+/**
  * Clean up reference.
  */
 ydn.ui.MessageDialog.prototype.dispose = function() {
@@ -69,6 +79,7 @@ ydn.ui.MessageDialog.prototype.dispose = function() {
   for (var i = buttons.length - 1; i >= 0; i--) {
     buttons[i].onclick = null;
   }
+  this.dialog_.onclose = null;
   document.body.removeChild(this.dialog_);
   this.dialog_ = null;
 };
@@ -87,10 +98,14 @@ ydn.ui.MessageDialog.showModal = function(title, message, opt_btn) {
   var dialog = new ydn.ui.MessageDialog(title, message, [ydn.ui.MessageDialog.Button.OK]);
   var df = new goog.async.Deferred();
   var buttons = dialog.dialog_.querySelector('.button-bar');
-  var default_btn = buttons.querySelector('button[value="apply"]');
+  var default_btn = buttons.querySelector('button.default');
   default_btn.onclick = function(e) {
     dialog.dialog_.close('apply');
     dialog.dispose_();
+  };
+  dialog.dialog_.onclose = function(event) {
+    df.resolve(dialog.dialog_.returnValue);
+    dialog.dispose();
   };
   dialog.dialog_.showModal();
   return df;
